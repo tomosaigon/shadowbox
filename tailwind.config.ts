830c1bd5005f6965287457166d0526f4d6b81772
tomo <tomo@crowdeo.com>
2025-02-26 13:21:38 +0700

    build: npm install @heroui...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+const { heroui } = require("@heroui/react");
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}", // Ensure Hero UI styles are included
   ],
   theme: {
     extend: {
@@ -20,9 +22,10 @@ export default {
     },
     {
       pattern: /bg-(blue|yellow|amber|red|green|purple)-(600)/,
-      variants: ['hover'],
+      variants: ["hover"],
     },
-    'disabled:bg-gray-400',
+    "disabled:bg-gray-400",
   ],
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()], // Add Hero UI plugin
 } satisfies Config;
